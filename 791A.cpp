030b766e9330729c137e0d32d2b925672701db7c
BrilliantAdam <v_-b@hotmail.com>
2024-05-23 06:26:38 +0300

    Add files via upload

---
+++ 791A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int a, b; // weights of Limak and Bob
+    std::cin >> a >> b;
+
+    int years = 0; // number of years
+
+    while (a <= b) {
+        a *= 3; // Limak's weight triples
+        b *= 2; // Bob's weight doubles
+        years++; // increment years
+    }
+
+    std::cout << years << std::endl;
+
+    return 0;
+}
