5a0ab6ab80db74501c321c761bf7d8470739bab9
BrilliantAdam <v_-b@hotmail.com>
2024-06-12 01:24:29 +0300

    Create 1985A.cpp

---
+++ 1985A.cpp
@@ -0,0 +1,16 @@
+//brilliantadam
+//11/6/2024
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string a, b;
+        cin >> a >> b;
+        swap(a[0], b[0]);
+        cout << a << " " << b << endl;
+    }
+    return 0;
+}
