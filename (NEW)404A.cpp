fab64bd96d43dc6c576e1a9591767088d4a8f92d
BrilliantAdam <v_-b@hotmail.com>
2024-06-25 15:20:18 +0300

    Update (NEW)404A.cpp

--- (NEW)404A.cpp
+++ (NEW)404A.cpp
@@ -1,5 +1,5 @@
 //brilliantadam
-//25/6/2024
+//5/6/2024
 #include<bits/stdc++.h>
 using namespace std;
  int cntDistinct(string str)
@@ -10,8 +10,6 @@ using namespace std;
     }
     return s.size();
 }
-
-
 int main() {
     int n , l;
     cin >> n;
@@ -20,15 +18,13 @@ int main() {
     for(int x=0 , j=l; x<n; x++, j--){
         string s;
         cin >> s;
-        ((x != j)? s1 += (s[x], s1+= s[j])  : s1 += s[j]);
+        ((x != j)? s1 += s[x], s1+= s[j]  : s1 += s[j]);
         s[x] = ' ';s[j] = ' ';
         s.erase(remove(s.begin(), s.end(), ' '), s.end());
         s2 += s;
     }
-    // cout << ((cntDistinct(s1) == 1 && cntDistinct(s2) == 1 && s1[0] != s2[0])? "YES" : "NO");
-
-    //Failed to delete diagonal values from string.
-    cout << "s1 " << s1 << " s2: " << s2 << endl;
-    cout << cntDistinct(s1) << " " << cntDistinct(s2) << " => " << s1[0] << " : " << s2[0];
+    cout << ((cntDistinct(s1) == 1 && cntDistinct(s2) == 1 && s1[0] != s2[0])? "YES" : "NO");
+    // cout << "s1 " << s1 << " s2: " << s2 << endl;
+    // cout << cntDistinct(s1) << " " << cntDistinct(s2) << " => " << s1[0] << " : " << s2[0];
     return 0;
 }
