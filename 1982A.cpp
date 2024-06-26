9b9a145b00ce4bedcacae5bb76021864e25b6598
BrilliantAdam <v_-b@hotmail.com>
2024-06-26 08:15:40 +0300

    Update 1982A.cpp

--- 1982A.cpp
+++ 1982A.cpp
@@ -14,3 +14,40 @@ int main()
     }
     return 0;
 }
+//####################
+//the previous code >>
+//####################
+// #include <bits/stdc++.h>
+// using namespace std;
+ 
+// int main()
+// {
+//     int t, x1 , y1 , x2 , y2;
+//     cin >> t;
+//     while(t--){
+//         cin >> x1 >> y1 >> x2 >> y2;
+//         if(x1 < y1){
+//             if(x2 >= y1 && y2 <= x2){
+//                 cout << "NO" << endl;
+//                 continue;
+//             }
+//             else{
+//                 cout << "YES"<< endl;
+//                 continue;
+//             }
+//         }
+//         else{
+//             if(y2 >= x1 && x2 <= y2 ){
+//                 cout << "NO"<< endl;
+//                 continue;
+//             }
+//             else{
+//                 cout << "YES"<< endl;
+//                 continue;
+//             }  
+//         }
+        
+//     }
+ 
+//     return 0;
+// }
