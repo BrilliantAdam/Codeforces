22ae2701a4177a0f57d716bbcafe10530554d37b
BrilliantAdam <v_-b@hotmail.com>
2024-06-28 17:14:32 +0300

    Create 227B.cpp

---
+++ 227B.cpp
@@ -0,0 +1,26 @@
+//brilliantadam
+//28/6/2024
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m , x {0} , y {0} , b;
+    cin >> n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin >> a[i];    
+    }
+    cin >> m;
+    for(int i=0; i<m; i++){
+        cin >> b;
+        for(int j=0 ; j < n; j++){
+            if(a[j] == b){
+                x += j+1;
+                y += (n-j);
+            }
+        }
+    }
+    cout << x << " " << y;
+    
+    return 0;
+}
