ec398f7c83c9b8810c12abf2819d5fb94db31b21
BrilliantAdam <v_-b@hotmail.com>
2024-10-04 10:06:24 +0300

    Create 1760c.cpp

---
+++ 1760c.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int a[n+5],b[n+5];
+		for(int i=1;i<=n;i++){
+			cin>>a[i];
+			b[i]=a[i];
+		}
+		sort(b+1,b+n+1);
+		for(int i=1;i<=n;i++){
+			if(a[i]==b[n])cout<<-b[n-1]+a[i]<<' ';
+			else cout<<-b[n]+a[i]<<" ";
+		}
+		cout<<endl;
+	}	
+}
