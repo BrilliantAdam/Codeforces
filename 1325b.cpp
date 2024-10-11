8d3b894336d90e4d145eb4c93106147c0f6535f7
BrilliantAdam <v_-b@hotmail.com>
2024-10-11 14:29:44 +0300

    Create 1325b.cpp

---
+++ 1325b.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+    int main(){
+        int t;
+        cin>>t;
+        while(t--){
+        int n;
+        cin>>n;
+        set<int>s1;
+            for(int i=0;i<n;i++){
+            	int x;
+            	cin>>x;
+            	s1.insert(x);
+            }
+        cout<<s1.size()<<'\n';
+    }
+}
