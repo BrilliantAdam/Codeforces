937b94b9ffa5114f6263b7e33a6589d2baf3c70f
BrilliantAdam <v_-b@hotmail.com>
2024-10-14 15:21:11 +0300

    Create 1385c.cpp

---
+++ 1385c.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n+1]={0};
+		for(int i=1;i<=n;i++){
+		  cin>>a[i];  
+		}
+		
+		
+		int j=n;
+		
+	
+			while(j>1 && a[j-1]>=a[j])
+			j--;
+			
+			while(j>1 && a[j-1]<=a[j])
+			j--;
+		
+		cout<<j-1<<"\n";
+			
+	}
+	return 0;
+}
