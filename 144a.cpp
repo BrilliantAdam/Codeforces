447a611ed6913490d3a7f140ad8d8ffb502624ee
BrilliantAdam <v_-b@hotmail.com>
2024-10-30 03:59:10 +0300

    Create 144a.cpp

---
+++ 144a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x;
+    int maxVal = -1, minVal = 100;
+    int maxIndex = 0, minIndex = 0;
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> x;
+        if (x > maxVal) {
+            maxVal = x;
+            maxIndex = i;
+        }
+        if (x <= minVal) {
+            minVal = x;
+            minIndex = i;
+        }
+    }
+
+    int result = maxIndex + (n - minIndex) - 1 - (maxIndex > minIndex);
+    cout << result;
+
+    return 0;
+}
